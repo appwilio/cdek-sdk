78b90be02568663223ff10d30461f02dd0a4ad85
Vladimir Tikunov <vtikunov@yandex.ru>
2018-07-24 10:47:45 +0300

    Добавлены скобки

--- src/Common/Order.php
+++ src/Common/Order.php
@@ -449,7 +449,7 @@ class Order
      */
     public function getSendCityCode(): ?int
     {
-        return $this->SendCity ? $this->SendCity->getCode() ?: null : null;
+        return $this->SendCity ? ($this->SendCity->getCode() ?: null) : null;
     }
 
     /**
@@ -475,7 +475,7 @@ class Order
      */
     public function getRecCityCode(): ?int
     {
-        return $this->RecCity ? $this->RecCity->getCode() ?: null : null;
+        return $this->RecCity ? ($this->RecCity->getCode() ?: null) : null;
     }
 
     /**
